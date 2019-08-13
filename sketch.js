43ccc8a6452d6c7826cb616a04f7d7c4de71f9c1
David Currie <david.alexander.currie@gmail.com>
2019-08-14 00:28:23 +1200

    button added with each day

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
+var button = [];
+
 function setup() {
   // put setup code here
+  var time = new Date();
+  var myClock = time.getTime().toString();
+  var myDay = Math.floor(myClock / 86400000);
+  var daysFromStart = myDay - 18121;
+  // var daysFromStart = myDay - 18121;
+  for (var i = 0; i < daysFromStart; i++) {
+    button[i] = createButton("Day " + (i + 1));
+  }
 }
 
 function draw() {
