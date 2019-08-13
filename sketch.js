1b2edea86ecc87f14f9bfde01e4552ddc12a021a
David Currie <david.alexander.currie@gmail.com>
2019-08-14 10:51:01 +1200

    codeSnippets json + auto create button daily

--- sketch.js
+++ sketch.js
@@ -1,17 +1,22 @@
 var button = [];
+var codeSnippets;
+
+function preload() {
+  codeSnippets = loadJSON("codeSnippets.json");
+}
 
 function setup() {
   // put setup code here
   var time = new Date();
   var myClock = time.getTime().toString();
   var myDay = Math.floor(myClock / 86400000);
-  var daysFromStart = myDay - 18121;
-  // var daysFromStart = myDay - 18121;
+  var daysFromStart = myDay - 18118;
+
   for (var i = 0; i < daysFromStart; i++) {
     button[i] = createButton("Day " + (i + 1));
+    createP("zozo");
+    createP(codeSnippets.zozo[i]);
+    createP("dave");
+    createP(codeSnippets.dave[i]);
   }
-}
-
-function draw() {
-  // put drawing code here
 }
