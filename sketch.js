3786083a00c909d8ed4cb753aaffa26926fb481a
David Currie <david.alexander.currie@gmail.com>
2019-08-15 16:25:55 +1200

    reset days

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function setup() {
   var time = new Date();
   var myClock = time.getTime().toString();
   var myDay = Math.floor(myClock / 86400000);
-  var daysFromStart = myDay - 18121;
+  var daysFromStart = myDay - 18123;
   createButtonFunctions(daysFromStart);
 
   for (var i = daysFromStart; i >= 0; i--) {
