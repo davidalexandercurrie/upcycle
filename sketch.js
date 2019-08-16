f6f3d6a951e1f91128868d7fdbfc18040eb8d972
David Currie <david.alexander.currie@gmail.com>
2019-08-16 12:07:01 +1200

    changing day value

--- sketch.js
+++ sketch.js
@@ -30,7 +30,8 @@ function setup() {
   var time = new Date();
   var myClock = time.getTime().toString();
   var myDay = Math.floor(myClock / 86400000);
-  var daysFromStart = myDay - 18122;
+  var daysFromStart = myDay - 18123;
+
   createButtonFunctions(daysFromStart);
 
   for (var i = daysFromStart; i >= 0; i--) {
